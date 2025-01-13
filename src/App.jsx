16a7acf9a182c85b9c9b725ff769dd8f2eeb46c2
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-01-12 20:07:16 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,58 @@
-import { useState } from 'react'
 import './App.css'
+import axios from 'axios';
+import { useState, useEffect } from 'react'
 
 function App() {
 
+  const url = 'http://localhost:3000/produtos'
+
   const [id, setId] = useState('');
-  const [Produto, setProduto] = useState('');
-  const [Valor, setValor] = useState('');
-  const [Quantidade, setQuantidade] = useState('');
-  const [Foto, setFoto] = useState('');
+  const [produto, setProduto] = useState('');
+  const [valor, setValor] = useState('');
+  const [quantidade, setQuantidade] = useState('');
+  const [foto, setFoto] = useState('');
 
   const [classInserir, setClassInserir] = useState('');
   const [classSalvar, setClassSalvar] = useState('sumir');
+  const [data, setData] = useState([]);
 
+  /** Carregar dados do banco */
+  useEffect(() => {
+    axios.get(url)
+      .then(res => setData(res.data))
+  }, [data, setData]);
+
+  /* Inserir dados no banco */
+  const Inserir = () => {
+    axios.post(url, {
+      produto, valor, quantidade, foto
+    })
+  }
+
+  /**Cadastrando dados no banco */
   const Cadastrar = (e) => {
     e.preventDefault()
 
-    if(Produto === ''){
+    if (produto === '') {
       alert("Por favor preencha o campo nome do produto")
-    }else if(Valor === ''){
+    } else if (valor === '') {
       alert("Por favor preencha o valor do produto")
-    }else if(Quantidade === ''){
+    } else if (quantidade === '') {
       alert("Por favor preencha o campo quantidade do produto")
-    }else if(Foto === ''){
+    } else if (foto === '') {
       alert("Por favor coloque a url da foto")
-    }else{
+    } else {
       alert("Produto cadastrado com sucesso!")
+      Inserir()
+
+      setProduto('')
+      setValor('')
+      setQuantidade('')
+      setFoto('')
     }
   }
+
+  /** Remover dados do banco */
 
   return (
 
@@ -39,7 +65,7 @@ function App() {
           <div className="col">
             <input
               type="text"
-              value={Produto}
+              value={produto}
               className='form-control'
               placeholder='Nome do Produto'
               onChange={(e) => setProduto(e.target.value)}
@@ -48,7 +74,7 @@ function App() {
           <div className="col">
             <input
               type="text"
-              value={Valor}
+              value={valor}
               placeholder='Valor'
               className='form-control'
               onChange={(e) => setValor(e.target.value)}
@@ -57,7 +83,7 @@ function App() {
           <div className="col">
             <input
               type="text"
-              value={Quantidade}
+              value={quantidade}
               placeholder='Qtd'
               className='form-control'
               onChange={(e) => setQuantidade(e.target.value)}
@@ -69,17 +95,17 @@ function App() {
           <div className="col">
             <input
               type="text"
-              value={Foto}
-              placeholder='Url da imagem'
+              value={foto}
+              placeholder='url da imagem'
               className='form-control'
-              onChange={ (e) => setFoto(e.target.value)}
+              onChange={(e) => setFoto(e.target.value)}
             />
           </div>
         </div>
 
         <button className={`btn btn-outline-success ${classInserir}`}>Inserir</button>
         <button className={`btn btn-outline-primary ${classSalvar}`}>Salvar</button>
-        
+
       </form>
 
       <table className='table table'>
@@ -93,119 +119,43 @@ function App() {
             <th scope='col' className='text-center' >Ações</th>
           </tr>
         </thead>
-        
+
         <tbody>
-          <tr>
-            <td scope='row'>1</td>
-            <td>Dell G15</td>
-            <td>$4.000</td>
-            <td>6</td>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td scope='row'>{item.id}</td>
+              <td>{item.produto}</td>
+              <td>{item.valor}</td>
+              <td>{item.quantidade}</td>
 
-            <td>
-              <img width={40} src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/g-series/g15-5530/media-gallery/gray/non-touch/coral/notebook-laptop-g15-5530-gray-gallery-1-v2.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=554&qlt=100,1&resMode=sharp2&size=554,402&chrss=full" alt="Imagem do produto" />
-            </td>
+              <td>
+                <img
+                  width={40}
+                  src={item.foto}
+                  alt="Imagem do produto"
+                />
+              </td>
 
-            <td>
+              <td>
 
-              <div className="btn-group d-flex gap-1">
+                <div className="btn-group d-flex gap-1">
 
-                <div className="btn btn-outline-warning">
-                  <i className="fa-solid fa-pen-to-square"></i>
+                  <div className="btn btn-outline-warning">
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </div>
+
+                  <div className="btn btn-outline-danger">
+                    <i className="fa-solid fa-trash"></i>
+                  </div>
+
                 </div>
 
-                <div className="btn btn-outline-danger">
-                  <i className="fa-solid fa-trash"></i>
-                </div>
+              </td>
 
-              </div>
+            </tr>
+          ))
 
-            </td>
-
-          </tr>
-
-          <tr>
-            <td scope='row'>2</td>
-            <td>iPhone 15</td>
-            <td>$5.000</td>
-            <td>8</td>
-
-            <td>
-              <img width={40} src="https://m.media-amazon.com/images/I/416MG51rNgL._AC_SX679_.jpg" alt="Imagem do produto" />
-            </td>
-
-            <td>
-
-              <div className="btn-group d-flex gap-1">
-
-                <div className="btn btn-outline-warning">
-                  <i className="fa-solid fa-pen-to-square"></i>
-                </div>
-
-                <div className="btn btn-outline-danger">
-                  <i className="fa-solid fa-trash"></i>
-                </div>
-
-              </div>
-
-            </td>
-
-          </tr>
-
-          <tr>
-            <td scope='row'>3</td>
-            <td>Apple What</td>
-            <td>$6.000</td>
-            <td>4</td>
-
-            <td>
-              <img width={40} src="https://http2.mlstatic.com/D_NQ_NP_2X_935658-MLA80209058319_102024-F.webp" alt="Imagem do produto" />
-            </td>
-
-            <td>
-
-              <div className="btn-group d-flex gap-1">
-
-                <div className="btn btn-outline-warning">
-                  <i className="fa-solid fa-pen-to-square"></i>
-                </div>
-
-                <div className="btn btn-outline-danger">
-                  <i className="fa-solid fa-trash"></i>
-                </div>
-
-              </div>
-
-            </td>
-
-          </tr>
-
-          <tr>
-            <td scope='row'>4</td>
-            <td>Monitor Gamer</td>
-            <td>$3.500</td>
-            <td>2</td>
-
-            <td>
-              <img width={40} src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/peripherals/monitors/alienware/aw2724dm/media-gallery/monitor-alienware-aw2724dm-black-gallery-8.psd?fmt=png-alpha&pscan=auto&scl=1&hei=804&wid=815&qlt=100,1&resMode=sharp2&size=815,804&chrss=full" alt="Imagem do produto" />
-            </td>
-
-            <td>
-
-              <div className="btn-group d-flex gap-1">
-
-                <div className="btn btn-outline-warning">
-                  <i className="fa-solid fa-pen-to-square"></i>
-                </div>
-
-                <div className="btn btn-outline-danger">
-                  <i className="fa-solid fa-trash"></i>
-                </div>
-
-              </div>
-
-            </td>
-
-          </tr>
+          }
         </tbody>
       </table>
 
